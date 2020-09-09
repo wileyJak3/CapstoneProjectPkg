4df5bb767566e487edce0e4f810f332c7c79e6d0
wileyJak3 <wileyJak3@gmail.com>
2020-09-09 09:55:43 -0400

    objects

---
+++ testapp/src/components/objects/preInvItems.js
@@ -0,0 +1,45 @@
+
+
+//imported objects that stores preset items to add to cart
+let preInvObj = 
+
+
+[
+    {
+    preInvImg: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone11-red-select-2019?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1566956144763",
+    preInvName:"Iphone 11",
+    preInvPrice:299
+},
+{
+    preInvImg: "https://images-na.ssl-images-amazon.com/images/I/51ZdmnHKukL._AC_SL1000_.jpg",
+    preInvName:"Fire TV Stick",
+    preInvPrice:29.99
+
+},
+{
+    preInvImg: "https://images-na.ssl-images-amazon.com/images/I/71zny7BTRlL._AC_SL1500_.jpg",
+    preInvName:"Apple AirPods Pro",
+    preInvPrice:219.98
+
+},
+{
+    preInvImg: "https://images-na.ssl-images-amazon.com/images/I/61445UADu9L._AC_SL1000_.jpg",
+    preInvName:"Nintendo Switch 32GB Console",
+    preInvPrice:411.90
+
+},
+{
+    preInvImg: "https://images-na.ssl-images-amazon.com/images/I/71XHuIVqJbL._AC_SL1500_.jpg",
+    preInvName:"Xbox One S 1TB Console",
+    preInvPrice:368.99
+
+},
+{
+    preInvImg: "https://images-na.ssl-images-amazon.com/images/I/71PGvPXpk5L._AC_SL1500_.jpg",
+    preInvName:"PlayStation 4 Slim 1TB Console",
+    preInvPrice:323.87
+
+}
+]
+
+export {preInvObj};
