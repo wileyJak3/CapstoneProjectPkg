d0a80e5363ca5a791eae7369e3403218814b4251
wileyJak3 <wileyJak3@gmail.com>
2020-09-09 10:01:11 -0400

    Home.js

---
+++ testapp/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//Home page component 
+
+function Home() {
+    return (
+        <div className = "home__cover" >
+            
+        </div>
+    )
+}
+
+export default Home
